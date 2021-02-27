411463057679f233c5d8044305eed0fe29b537b9
David Boland <dboland77@gmail.com>
2021-02-27 10:14:39 +0000

    Set up and started express server

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 
+const app = express();
+
+const PORT = process.env.PORT || 8888;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
