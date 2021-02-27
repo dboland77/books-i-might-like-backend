3301b17873eaa96d58408f5f33678d5b19b9ceaf
David Boland <dboland77@gmail.com>
2021-02-27 13:41:01 +0000

    Add user validation

--- routes/router.js
+++ routes/router.js
@@ -3,10 +3,11 @@ import bcrypt from "bcryptjs";
 import { v4 as uuid } from "uuid";
 import jwt from "jsonwebtoken";
 import db from "../config/db.js";
+import validateRegister from "../middleware/users.js"
 
 const router = express.Router();
 
-router.post("/sign-up", (req, res, next) => {});
+router.post("/sign-up", validateRegister, (req, res, next) => {});
 
 router.post("/login", (req, res, next) => {});
 
